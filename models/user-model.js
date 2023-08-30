f33d38758b983e20e39f0dfc7477a09f4d585e95
sheraz <sherazzafar148@gmail.com>
2023-08-30 16:49:07 +0500

    ok

---
+++ models/user-model.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+    {
+        phone: { type: String, required: true },
+        name: { type: String, required: false },
+        avatar: {
+            type: String,
+            required: false,
+            get: (avatar) => {
+                if (avatar) {
+                    return `/images/monkey-avatar.png`;
+                }
+                return avatar;
+            },
+        },
+        activated: { type: Boolean, required: false, default: false },
+    },
+    {
+        timestamps: true,
+        toJSON: { getters: true },
+    }
+    
+);
+
+module.exports = mongoose.model('User', userSchema, 'users');
